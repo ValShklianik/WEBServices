97e78092723f965190accb8a9b8a56f8ec5ff5bc
ValShklianik <val.shklianik@gmail.com>
2018-12-24 00:28:50 +0300

    added lambda function

---
+++ app/FibonacciLambda/FibonacciLambda.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using Amazon.Lambda.Core;
+
+namespace FibonacciLambda
+{
+    public class Function
+    {
+        class InputValue
+        {
+            public long input {get; set;}
+        } 
+
+        class ResultValue
+        {
+            public List<long> result {get; set;}
+        } 
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.Json.JsonSerializer))]
+        public List<long> Handler(InputValue input)
+        {
+            var list = new List<long>();
+            list.Add(1);
+            list.Add(2);
+            return new ResultValue()
+            {
+                result = list;
+            };
+        }
+    }
+}
